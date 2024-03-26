45bf289919c96e06ce6ac3ab621b2a96b6a2d0fc
jaehoonmandev <wido1593@gmail.com>
2024-03-26 15:00:31 +0900

    1. side Functions button 스크롤 액션 추가

--- src/app/components/UI/sideFunctions/sideFunctions.tsx
+++ src/app/components/UI/sideFunctions/sideFunctions.tsx
@@ -1,8 +1,24 @@
 import styles from "@/app/components/UI/sideFunctions/sideFunctions.module.css";
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export default function SideFunctions({toggleHandler}:{toggleHandler:()=> void}) {
 
+    const [showButton, setShowButton] = useState(false);
+
+    //일정 위치 이상 스크롤 했을 때만 보여주기
+    useEffect(() => {
+        const ShowButtonClick = () => {
+            if (window.scrollY > 100) {
+                setShowButton(true)
+            } else {
+                setShowButton(false)
+            }
+        }
+        window.addEventListener("scroll", ShowButtonClick)
+        return () => {
+            window.removeEventListener("scroll", ShowButtonClick)
+        }
+    }, [])
 
     //상단으로 스크롤 이동
     const scrollToTop = () => {
@@ -16,14 +32,15 @@ export default function SideFunctions({toggleHandler}:{toggleHandler:()=> void})
 
     return (
 
-        <div className={styles.sideFunctions}>
+            <div className={`${styles.sideFunctions} ${showButton && styles.active } `}>
 
-            <button className={`${styles.scrollToTop} ${styles.background}`} onClick={scrollToTop}>
-            </button>
+                <button className={`${styles.scrollToTop} ${styles.background}`} onClick={scrollToTop}>
+                </button>
 
-            <button className={`${styles.sideAddGuestBook} ${styles.background}`} onClick={toggleHandler}>
-            </button>
+                <button className={`${styles.sideAddGuestBook} ${styles.background}`} onClick={toggleHandler}>
+                </button>
 
-        </div>
+            </div>
+
     )
 }

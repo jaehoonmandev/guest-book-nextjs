6bce00f1df0bc6e218ad7b23db46c74b4867ba32
jaehoonmandev <wido1593@gmail.com>
2024-03-26 17:04:54 +0900

    1. moment 사용 시 format 관련 warning 수정

--- src/app/utility/dateConvert.tsx
+++ src/app/utility/dateConvert.tsx
@@ -1,19 +1,16 @@
 import moment from "moment-timezone";
 
+
+
+
+//사용자의 위치를 유추하는 기능을 활성화한다.
+const localTimeZone = moment.tz.guess(true);
+
+
 /**
  * 날짜 형식 변환 유틸.
  * @param date
  */
 export const dateConvert = (date : Date) => {
-
-    //사용자의 위치를 유추하는 기능을 활성화한다.
-    const localTimeZone = moment.tz.guess(true);
-
-    //timezone에 맞게 date를 변환
-    const dateOnTimeZone = moment.utc(date).tz(localTimeZone);
-
-    //formatting
-    const formattedDate = dateOnTimeZone.format("YYYY-MM-DD HH:mm:ss");
-
-    return formattedDate
+    return moment.utc(date).tz(localTimeZone).format("YYYY-MM-DD HH:mm:ss");
 }

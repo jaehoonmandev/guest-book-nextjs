cd6685babfd1859d66dd34d4390cb26c7e5b15da
jaehoonmandev <wido1593@gmail.com>
2024-03-26 14:03:43 +0900

    1. 방명록 추가 모달 띄우기 function 부모로 올려서 공유

--- src/app/components/guest-book/addGuestBook.tsx
+++ src/app/components/guest-book/addGuestBook.tsx
@@ -5,31 +5,13 @@ import {createPortal} from "react-dom";
 import {AddGuestBookProps} from "@/app/interfaces/guestBook";
 
 
-export default function AddGuestBook({isLoading = true, guestBookLength = 0}:AddGuestBookProps) {
+export default function AddGuestBook({toggleHandler,isModalOpen}:AddGuestBookProps) {
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
     let [portalElement, setPortalElement] = useState<Element | null>(null);
 
     useEffect(() => {
         setPortalElement(document.getElementById("portal"));
     }, [isModalOpen]);
-
-    const toggleHandler = () => {
-        if(!isLoading || guestBookLength > 0){
-
-            setIsModalOpen((prevIsModalOpen) => {
-                //이전 상태가 확장 상태가 아니라면 즉, 현재 사이드바를 확장하는 중이라면
-                if(prevIsModalOpen === false){
-                    // body 스크롤을 방지한다
-                    document.body.style.overflow = 'hidden';
-                }else {
-                    //사이드를 닫는다면 스크롤을 다시 솰성 시킨다.
-                    document.body.style.removeProperty('overflow');
-                }
-                return !prevIsModalOpen
-            });
-        }
-    }
 
     return (
         <>
